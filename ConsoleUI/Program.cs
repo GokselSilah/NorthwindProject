4f6f12682c82dcfacc889f46a9b7a66d761269c7
göksel silah <gokselsilah@hotmail.com>
2021-12-20 03:11:57 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            //foreach (var product in productManager.GetAll())
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            //var result = productManager.GetAllByCategoryId(2);
+
+            //foreach (var product in result)
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            var result2 = productManager.GetByUnitPrice(50, 100);
+
+            foreach (var product in result2)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
